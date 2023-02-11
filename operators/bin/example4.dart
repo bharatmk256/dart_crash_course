f557ca0f1fd784af0283c0d5db78c4e713482d45
Bharat Makwana <bharatmk257@gmail.com>
2023-02-11 19:18:43 +0530

    added example 4 operators

---
+++ operators/bin/example4.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  //Binary infix operators
+  const age = 50;
+  print(age + 20);
+  print(age - 20);
+  print(age * 20);
+  print(age / 20);
+  print(age ~/ 20);
+  print(age % 20);
+  print(age == 20);
+}
