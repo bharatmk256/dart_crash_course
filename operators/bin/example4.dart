dab32a62253376653de1e2dec81c2da669ad9bfe
Bharat Makwana <bharatmk257@gmail.com>
2023-02-12 10:55:02 +0530

    operators

--- operators/bin/example4.dart
+++ operators/bin/example4.dart
@@ -6,6 +6,30 @@ void main(List<String> args) {
   print(age * 20);
   print(age / 20);
   print(age ~/ 20);
-  print(age % 20);
+  print(age % 20); //Remainder
   print(age == 20);
+  print(age != 20);
+
+  print(age > 20); // true
+  print(age < 20); // false
+  print(age >= 20); // true
+  print(age <= 20); // false
+
+  // bitwise infix operators
+  print(age & 20); // bitwise And 1 & 1 = 1, 0 & 1 = 0 & 1 & 0 = 0, 0 & 0 = 0
+
+  /// 0000 1010
+  /// 1011 1000
+  /// ---and---
+  /// 0000 1000
+
+  print(age | 20); // bitwise OR 1 | 1 = 1, 0 | 1 = 1, 1 | 0 = 1, 0 | 0 = 0
+  print(age ^ 20); // bitwise XOR 1 | 1 = 0, 0 | 1 = 1, 1 | 0 = 1, 0 | 0 = 0
+
+  print(age << 20); // bitwise left shift
+  print(age >> 20); // bitwise rigght shift
+
+  /// 0110 0011
+  /// shift left by 1
+  /// 1100 0110
 }
---
+++ operators/bin/example6.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  // print('Hello' + 2);
+  var myAge = 20;
+  print(myAge = 30); // myAge = 30
+  print(myAge ~/= 2); // myAge = 15
+  print(myAge *= 2); // myAge = 30
+  print(myAge += 4); // myAge = 34
+  print(myAge &= 2); // myAge = 2
+  print(myAge |= 6); // myAge = 6
+  print(myAge ^= 10); // myAge = 12
+  print(myAge -= 10); // myAge = 2
+}
